544325905a29742af86d42d52e3b9597907a3b90
Shatabda-Roy <Shatabdaroy@gmail.com>
2023-10-12 21:41:58 +0600

    console support for stderr

--- appMain.cpp
+++ appMain.cpp
@@ -1,15 +1,20 @@
-#include "appWnd.h"
 #include "appDevice.h"
-
 HWND        g_hWnd;
-HINSTANCE   g_hInstance;
 bool g_shouldClose = false;
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int main () {    
-    apparatus::InitWindow _window{g_hInstance, g_hWnd, SW_SHOW, WindowProc};
-    apparatus::InitVulkan _vulkan{};
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+	AllocConsole();
+	FILE* fDummy;
+	freopen_s(&fDummy, "CONOUT$", "w", stdout);
+    freopen_s(&fDummy, "CONOUT$","w",stderr);
+    std::cerr.clear();
+	std::cout.clear();
+    apparatus::InitWindow _window{hInstance, SW_SHOW, WindowProc};
+    g_hWnd = _window.g_hWnd;
+    apparatus::InitVulkan _vulkan{g_hWnd,hInstance};
     
     MSG msg = { };
     
